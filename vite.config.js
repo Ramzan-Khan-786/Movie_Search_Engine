eb46f57a3369cb0f7d17106fe22054afff22d019
KHAN RAMZAN B. <ramzankhan786929@gmail.com>
2025-10-23 10:04:41 +0530

    changed the output format

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Movie_Search_Engine/', // ← your GitHub repo name here
+  base: '/', // ← your GitHub repo name here
 })
